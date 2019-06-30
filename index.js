c523204d95e0a954ca5313bff233d5eabaa6f1ca
Ankush Jadhav <ankush@leadschool.in>
2019-06-30 20:46:19 +0530

    test

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: false }));
 
 app.post('/api/challenge', (req, res, next) => {
-	res.status(200).send({ "challenge": req.body.challenge });
+	console.log(`Recieved a request at /api/challenge`);
+	console.log(JSON.stringify(req.body));
+	return res.status(200).send({ "challenge": req.body.challenge });
 });
 
 app.set('SERVER_PORT', process.env.SERVER_PORT || 3000);
